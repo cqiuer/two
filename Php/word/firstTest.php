4e76e46988b76f999bec05ead3610aec53f106a4
cqiuer <freedomcqiu@163.com>
2018-10-10 13:45:30 +0800

    coding第一次测试

---
+++ Php/word/firstTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liexiang
+ * Date: 18-10-10
+ * Time: 上午11:06
+ */
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: liexiang
+ * Date: 18-10-10
+ * Time: 上午11:09
+ */
+phpinfo();
